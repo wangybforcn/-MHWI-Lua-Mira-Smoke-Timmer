be4dbc2591442a4533e3ca2604b71a5a4f635a8c
RosmontisQD <38135623+wangybforcn@users.noreply.github.com>
2024-03-07 12:09:55 +0900

    update
    update to use

--- MiraSmokeTimmer_.lua
+++ MiraSmokeTimmer_.lua
@@ -2,9 +2,10 @@
 missionSwitch = false          --任务开关
 SmokeTimmerSwitch = false      --函数开关
 
-_SmkActionSwitch = false        --烟雾中动作开关
 _10sSmkTimmerSwitch = false     --10s计时器开关
 _20sSmkCdTimmerSwitch = false       --20s计时器开关
+
+Mira = nil      --Mira data
 
 function MiraSmokeTimmer()
 
@@ -21,57 +22,55 @@ function MiraSmokeTimmer()
 
         --monster
         local monsterList = GetAllMonster()
-        local Mira = nil
+
         for monster, monsterData in pairs(monsterList) do
             local Data_Monster = engine.Monster:new(monster)
             Mira = Data_Monster
         end
-
+        
         --in smoke to do
-        if _SmkActionSwitch == false then
-            _SmkActionSwitch = true
-            if Mira.Action.lmtID == 16395 or Mira.Action.lmtID == 16396 or Mira.Action.lmtID == 16398 or Mira.Action.lmtID == 16397 then
-                
-                --10sTimmer start
-                if (0 < Mira.Frame.frame and Mira.Frame.frame < 10) and _10sSmkTimmerSwitch == false then
-                    Message("开始10秒烟雾弹隐匿计时...")
-                    _10sSmkTimmerSwitch = true
-                    AddChronoscope(8, "MiraInSmokeTimmer")
-                end
-
-                --10sTimmer end and 20sTimmer start
-                if (Mira.Action.lmtID == 16395 or Mira.Action.lmtID == 16396 or Mira.Action.lmtID == 16398 or Mira.Action.lmtID == 16397) and CheckChronoscope("MiraInSomkeTimmer") == false then
-                    if (580 < Mira.Frame.frame and Mira.Frame.frame < 590) and _10sSmkTimmerSwitch == true then
-                        _10sSmkTimmerSwitch = false
-                        DelChronoscope("MiraInSomkeTimmer")
-                        Message("黑龙即将脱离烟雾弹索敌阶段，请注意！")
-
-                        if not (Mira.Action.lmtID == 16395 or Mira.Action.lmtID == 16396 or Mira.Action.lmtID == 16398 or Mira.Action.lmtID == 16397) then
-                            if not _20sSmkCdTimmerSwitch then
-                                _20sSmkCdTimmerSwitch = true
-                                Message("现在开始20秒烟雾弹计时\nTips:请按照提示为准不要自己倒计时！")
-                                AddChronoscope(23, "MiraSmokeCDTimmer")
-                            end
-                        end
-                    end
-                end
-
-                --20sTimmer end
-                if not (Mira.Action.lmtID == 16395 or Mira.Action.lmtID == 16396 or Mira.Action.lmtID == 16398 or Mira.Action.lmtID == 16397) and not CheckChronoscope("MiraSmokeCDTimmer") then
-                    if _20sSmkCdTimmerSwitch == true then
-                        _20sSmkCdTimmerSwitch= false
-                        DelChronoscope("MiraSmokeCDTimmer")
-                        Message("黑龙20秒烟雾抗性已结束")
-                       
-                        _10sSmkTimmerSwitch = false
-                        _20sSmkCdTimmerSwitch = false
-                    end
-                end
+        if Mira.Action.lmtID == 16395 or Mira.Action.lmtID == 16396 or Mira.Action.lmtID == 16398 or Mira.Action.lmtID == 16397 then
+            --10sTimmer start
+            if (0 < Mira.Frame.frame and Mira.Frame.frame < 10) and _10sSmkTimmerSwitch == false then
+                Message("开始10秒烟雾弹隐匿计时...")
+                _10sSmkTimmerSwitch = true
+                AddChronoscope(8, "MiraInSmokeTimmer")
             end
-            _SmkActionSwitch = false
+
+            _10sTimmerChecker()
         end
+
+        _20sTimmerChecker()
     end
 end
+
+
+function _10sTimmerChecker()
+    if CheckChronoscope("MiraInSmokeTimmer") then
+        Message("黑龙即将脱离烟雾弹索敌阶段，请注意！")
+        AddChronoscope(23, "MiraSmokeCDTimmer")
+        Message("现在开始20秒烟雾弹CD计时\nTips:请按照提示为准不要自己倒计时！")
+        _20sSmkCdTimmerSwitch = true
+    elseif not CheckChronoscope("MiraInSmokeTimmer") and 
+    (not Mira.Action.lmtID == 16395 and 
+    not Mira.Action.lmtID == 16396 and 
+    not Mira.Action.lmtID == 16398 and
+    not Mira.Action.lmtID == 16397) then
+        Message("黑龙已提前脱离烟雾索敌状态！\n将直接开始20秒CD计时!")
+        AddChronoscope(23, "MiraSmokeCDTimmer")
+        _20sSmkCdTimmerSwitch = true
+    end
+end
+
+
+function _20sTimmerChecker()
+    if CheckChronoscope("MiraSmokeCDTimmer") then
+        Message("黑龙20秒烟雾抗性已结束")
+        _10sSmkTimmerSwitch = false
+        _20sSmkCdTimmerSwitch = false
+    end
+end
+
 
 
 
@@ -84,9 +83,13 @@ end
 
 --游戏每秒检测的代码
 function on_time()
-    if SmokeTimmerSwitch == false then
+    if SmokeTimmerSwitch == false and _10sSmkTimmerSwitch == false and _20sSmkCdTimmerSwitch == false then
         MiraSmokeTimmer()
         SmokeTimmerSwitch = false
+    elseif SmokeTimmerSwitch == false and _10sSmkTimmerSwitch == true and _20sSmkCdTimmerSwitch == false then
+        _10sTimmerChecker()
+    elseif SmokeTimmerSwitch == false and _10sSmkTimmerSwitch == true and _20sSmkCdTimmerSwitch == true then
+        _20sTimmerChecker()
     end
 end
 
@@ -97,8 +100,12 @@ end
 
 --每次生成怪物时执行的代码
 function on_monster_create()
+    _10sSmkTimmerSwitch = false
+    _20sSmkCdTimmerSwitch = false
 end
 
 --每次销毁怪物时执行的代码
 function on_monster_destroy()
+    _10sSmkTimmerSwitch = false
+    _20sSmkCdTimmerSwitch = false
 end
